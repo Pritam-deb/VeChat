f8da6313d0637c64aaffcbf9d3e35deb713c09de
Pritam <pritamdebnath297@gmail.com>
2021-05-30 17:53:21 +0530

    chore: build version

---
+++ build/precache-manifest.c6e9cb3625aaf51ce6bbe7859d75a0a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8827f38114b5c4bfdcd78e11e626a13e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cbe728d2b0d6c470432",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "27704a2d1a79ca793e4c",
+    "url": "/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "0cbe728d2b0d6c470432",
+    "url": "/static/js/2.b175279c.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.b175279c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27704a2d1a79ca793e4c",
+    "url": "/static/js/main.a4645687.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
