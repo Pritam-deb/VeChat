3a97e74739c6ee1c17ad9033ad392e2a8e87b2bf
Pritam <pritamdebnath297@gmail.com>
2021-05-30 12:00:06 +0530

    feat: created chat feed
    not redirecting to feed after login

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
+import { ChatEngine } from 'react-chat-engine';
+import {auth} from '../firebase';
+
 
 const Chats = () => {
+
+    const history=useHistory();
+
+    const handleLogout = async() => {
+        await auth.signOut();
+        history.push('/');
+    }
+
+
     return(
-        <h1>Chats</h1>
+        <div className="chats-page">
+            <div className="nav-bar">
+                <div className="logo-tab">
+                    VeChat!
+                </div>
+                <div onClick={handleLogout} className="logout-tab">
+                    Logout
+                </div>
+            </div>
+            <ChatEngine
+            
+                height="clac(100vh-66px)"
+                projectId="1611b74d-b16b-45f7-9364-ee7594ca2b8d"
+                userName="."
+                userSecret="."
+            />
+        </div>
     )
 }
 
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -8,18 +8,18 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
     const [loading, setLoading] = useState(true);
-    const [user, setUser] = useState({});
+    const [user, setUser] = useState(null);
     const history = useHistory();
 
     useEffect(() => {
         auth.onAuthStateChanged(()=> {
             setUser(user);
             setLoading(false);
-            history.push('/chats');
+            if(user) history.push('/chats');
         })
         
     }, [user, history])
-
+    
     const value = { user };
     return(
         <AuthContext.Provider value={value}>
